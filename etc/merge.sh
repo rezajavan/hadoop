fb50e1903db40d3a70c3edd43535f8bc04d02847
Reza Javan <rezajavanc2024@gmail.com>
2024-03-22 18:19:50 +0330

    merge.sh changed

--- etc/merge.sh
+++ etc/merge.sh
@@ -1,4 +1,4 @@
-cp mapred-site.xml $HADOOP_HOME/etc/hadoop/mapred-site.xml
-cp core-site.xml $HADOOP_HOME/etc/hadoop/core-site.xml
-cp hadoop-env.sh $HADOOP_HOME/etc/hadoop/hadoop-env.sh
-cp yarn-site.xml $HADOOP_HOME/etc/hadoop/yarn-site.xml
+cp mapred-site.xml $HADOOP_HOME/etc/hadoop/mapred-site.xml -v
+cp core-site.xml $HADOOP_HOME/etc/hadoop/core-site.xml -v
+cp hadoop-env.sh $HADOOP_HOME/etc/hadoop/hadoop-env.sh -v
+cp yarn-site.xml $HADOOP_HOME/etc/hadoop/yarn-site.xml -v
